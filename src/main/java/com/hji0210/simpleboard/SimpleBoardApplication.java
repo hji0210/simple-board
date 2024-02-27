e59954dbccd9b389c82145a684f020e553e9e6e8
jiin <jyl08840@naver.com>
2024-02-27 22:34:37 +0900

    initial commit

---
+++ src/main/java/com/hji0210/simpleboard/SimpleBoardApplication.java
@@ -0,0 +1,13 @@
+package com.hji0210.simpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hji0210/simpleboard/SimpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hji0210.simpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
